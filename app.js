cdd255c530f718fd1d4eb5de50b63f49e35bfc8d
Stepan Oryshchak <74045601+stepan-oryshchak@users.noreply.github.com>
2023-12-10 01:56:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname)); // Виправлення тут: __dirname замість dirname
+app.use(express.static(__dirname));
 app.use('/api', projectorRouter);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Виправлення тут: __dirname замість dirname
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер працює на порті http://localhost:${PORT}`); // Виправлення тут: використання обратних апострофів для відображення значення PORT
+    console.log(`Сервер працює на порті http://localhost:${PORT}`);
 });
